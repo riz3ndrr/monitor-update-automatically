9eafe326b57ead52c3c9836fe123b7022dc45919
riz3ndrr <riz3ndrr@gmail.com>
2025-11-14 13:14:45 +1300

    Initial commit :)

---
+++ detect_hdmi.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+hdmi="HDMI-2"
+
+if xrandr | grep -E -q "HDMI-2 connected"; then
+  prev=1
+else
+  prev=0
+fi
+
+while true; do
+  if xrandr | grep -E -q "HDMI-2 connected"; then
+    cur=1
+  else
+    cur=0
+  fi
+
+  if [ "$cur" -ne "$prev" ]; then
+    if [ $cur -eq 1 ]; then
+      echo "HDMI Connected to 2nd Monitor"
+      ./dual_mon.sh
+    elif [ $cur -eq 0 ]; then
+      echo "HDMI Disconnected, using only laptop monitor"
+      ./single_mon.sh
+    fi
+
+    prev=$cur
+  fi
+  sleep 5
+done
---
+++ dual_mon.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 760x1440 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --mode 3440x1440 --pos 0x0 --rotate normal
+sleep 1
+i3 restart
