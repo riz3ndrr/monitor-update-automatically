e20cec993382cb8ad04a927d4ddca1451a924111
riz3ndrr <riz3ndrr@gmail.com>
2025-11-18 10:28:46 +1300

    Made script slightly more efficient along with using another configuration for two monitors

--- detect_hdmi.sh
+++ detect_hdmi.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 hdmi="HDMI-2"
+dual_mon_sh="dual_mon_side_by_side.sh"
+single_mon_sh="single_mon.sh"
 
 if xrandr | grep -E -q "HDMI-2 connected"; then
   prev=1
   echo "HDMI Connected to 2nd Monitor"
-  $HOME/Scripts/update_monitor/dual_mon.sh
+  $HOME/Scripts/update_monitor/$dual_mon_sh
 else
   prev=0
   echo "HDMI Disconnected, using only laptop monitor"
-  $HOME/Scripts/update_monitor/single_mon.sh
+  $HOME/Scripts/update_monitor/$single_mon_sh
 fi
 
 while true; do
@@ -22,10 +24,10 @@ while true; do
   if [ "$cur" -ne "$prev" ]; then
     if [ $cur -eq 1 ]; then
       echo "HDMI Connected to 2nd Monitor"
-      $HOME/Scripts/update_monitor/dual_mon.sh
+      $HOME/Scripts/update_monitor/$dual_mon_sh
     elif [ $cur -eq 0 ]; then
       echo "HDMI Disconnected, using only laptop monitor"
-      $HOME/Scripts/update_monitor/single_mon.sh
+      $HOME/Scripts/update_monitor/$single_mon_sh
     fi
 
     prev=$cur
